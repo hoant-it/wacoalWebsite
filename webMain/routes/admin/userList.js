0bbb64188b7b508e508b893c8ba97840684fe09b
Nguyen Hoa <hoanguyen_it@wacoal.com>
2021-01-19 16:31:19 +0700

    them user list

--- webMain/routes/admin/userList.js
+++ webMain/routes/admin/userList.js
@@ -5,13 +5,30 @@ var router = express.Router();
 
 /* GET user page. */
 router.get('/', async (req,res) => {
+  var arrUserList=[];
+await db.query("wacoal_GetUserList_Web_V1",{
+
+}).then(result => {
+  arrUserList=result[0];
+  console.log(arrUserList);
+})
   res.render("admin/userList", {
     title:'Express',
     userId:req.signedCookies.userId,
-    html:req.signedCookies.html
+    html:req.signedCookies.html,
+    arrUserList:arrUserList
   });
 
-} )
+} );
+
+router.post('/',async(req,res) => {
+  const {bsubmit}= req.body;
+  if(bsubmit==="submitSave"){
+    console.log("dang save ne")
+  }else{
+    console.log("dang cancel ne")
+  }
+});
 
 
 
